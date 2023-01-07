a6d52ada7f19599e2c5c7b86792ff4c3d07a2274
Tiainaittes <etienne.pomente@reseau.sncf.fr>
2023-01-07 19:09:12 +0100

    m

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  register(createAuthDto: CreateAuthDto) {}
+}
