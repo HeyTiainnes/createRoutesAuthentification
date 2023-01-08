77527f03704a2f5224b84c8f6556379be272e033
Tiainaittes <etienne.pomente@reseau.sncf.fr>
2023-01-08 08:40:28 +0100

    mise en place user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,15 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
+//import { User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 
-@Injectable()
-export class AuthService {
-  constructor(
-    @InjectRepository(User)
-    private userRepository: Repository<User>
-  ) {}
+// @Injectable()
+// export class AuthService {
+//   constructor(
+//     @InjectRepository(User)
+//     private userRepository: Repository<User>
+//   ) {}
 
-  register(createAuthDto: CreateAuthDto) {}
-}
+//   register(createAuthDto: CreateAuthDto) {}
+// }
